786fdc8a75128060928bb79f4f54b6b8a7229d56
Omar Abohashim <ohashim530@gmail.com>
2026-08-07 06:10:22 +0300

    handle jackson exceptions

--- src/main/java/com/omar/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/omar/ecommerce/exception/GlobalExceptionHandler.java
@@ -4,11 +4,15 @@ import com.omar.ecommerce.dto.response.ApiResponse;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
+import tools.jackson.databind.exc.InvalidFormatException;
+import tools.jackson.databind.exc.MismatchedInputException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -57,6 +61,8 @@ public ResponseEntity<ApiResponse<Object>> handleInsufficientStock(InsufficientS
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponse.error("Validation failed", errors));
     }
+
+
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ApiResponse<Object>> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
@@ -78,5 +84,13 @@ public ResponseEntity<ApiResponse<Object>> handleInsufficientStock(InsufficientS
     public ResponseEntity<ApiResponse<Object>> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(ApiResponse.error("A database constraint was violated. The data may already exist or reference something invalid."));
+    }
+    // for jackson parse exception
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<Void>> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex) {
+
+        return ResponseEntity.badRequest()
+                .body(ApiResponse.error("Invalid request body."));
     }
 }
